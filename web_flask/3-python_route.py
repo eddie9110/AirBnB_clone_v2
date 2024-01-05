6d3198c06eca9b463575c4724e2e513f290acba6
eddie9110 <edwinmaregwa@gmail.com>
2024-01-05 00:42:39 -0800

    task 3&4 added

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,15 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """returns the home page"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
     return "HBNB!"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
@@ -23,12 +26,14 @@ def c(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
+
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", text="is cool", strict_slashes=False)
 def python_(text):
     """displays Python+text"""
     text = text.replace("_", " ")
     return f"Python {text}"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,36 +4,41 @@ script that starts a Flask web application
 """
 
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def home():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
     return "HBNB!"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """displays C"""
     return f"C {text}"
 
+
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_(text):
-    """displays Python+text""" 
+    """displays Python+text"""
     text = text.replace("_", " ")
     return f'Python + {text}'
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_(n):
     """n is a number"""
     if type(n) == int:
         return f"{n} is a number"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
