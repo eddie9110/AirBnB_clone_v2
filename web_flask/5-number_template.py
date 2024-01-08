3b08807ef14941c601390a90bbc4ff197549ad62
eddie9110 <edwinmaregwa@gmail.com>
2024-01-08 06:48:20 -0800

    task 6 added

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,20 +7,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
     return "HBNB!"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """displays C"""
     return f"C {text}"
+
 
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -29,15 +33,18 @@ def python_(text):
     text = text.replace("_", " ")
     return f'Python + {text}'
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_(n):
     """n is a number"""
         return f"{n} is a number"
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_temp(n):
     """renders a html tmplate"""
-        return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
